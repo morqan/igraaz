6a27e1f081e11e4d2b388bc98c9552f69350f986
Murad <qurbanbeyli.murad@gmail.com>
2019-08-02 18:56:06 +0400

    refactoring

--- src/js/main/ripple-effect.js
+++ src/js/main/ripple-effect.js
@@ -1,34 +1,3 @@
-$(document).ready(function(){
-    $('.main__carousel').owlCarousel({
-        loop:true,
-        dotsContainer: '#carousel-custom-dots',
-        navContainer:'#carousel-custom-nav',
-        navText : ['ƏVVƏLKİ','SONRAKI'],
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
-    });
-  });
-
-  $(document).ready(function() {
-    $(".myOwl__dot").click(function () {
-        $(".myOwl__dot").removeClass("myOwl__dot--active");
-        $(this).addClass("myOwl__dot--active");   
-    });
- });
-
 $( document ).ready(function() {
    // Ripple-effect animation
 (function($) {
