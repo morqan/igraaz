cb67b14240b6f9c50145512a2eab6f3acbd168fd
Murad <qurbanbeyli.murad@gmail.com>
2019-09-06 19:03:06 +0400

    refactoring

--- build/js/all.js
+++ build/js/all.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){        
+    //для контейнера с id #IDhtmlcont - 
+    Dropzone.options.IDhtmlcont = {
+      paramName: "file", // имя переменной, используемой для передачи файлов
+      maxFilesize: 2, // лимит размера файла в МБ
+      parallelUploads: 1, //кол-во параллельных обращений к серверу
+      maxFiles: 10, //макс. число файлов в данном контейнере
+      accept: function(file, done) {
+        //произвольная функция проверки загружаемых файлов
+        if (file.type == "image/png") {
+          //сообщение без ошибки, если файл забракован
+          done("I don`t accept PNGs.");
+        }
+        //чтобы файл был принят, нужно вызвать done без параметров
+        else { done(); }
+      }
+    };
+});
+
+
+
 $( document ).ready(function() {
     $("#bd").flatpickr({
         altInput: true,
